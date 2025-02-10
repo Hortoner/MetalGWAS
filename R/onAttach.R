e9fff6d7584e000ddeb15ae7247f6034c3ab826a
Hortoner <125968602+Hortoner@users.noreply.github.com>
2025-02-10 19:24:20 +0800

    Add files via upload

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,10 +1,9 @@
 .onAttach <- function(...) {
-  Version <- as.numeric(installed.packages()["QTLMR","Version"])
   message("")
   message("============================================================")
   message("")
   message(paste0("                      感谢小伙伴们的使用!"))
-  message(paste0("                更多POST GWAS分析请关注QTLMR包"))
+  message(paste0("          更多POST GWAS分析，请关注微信公众号：落叶随风的笔记"))
   message("               Dr.H E-mail: 2214047750@qq.com")
   message("")
   message("============================================================")
